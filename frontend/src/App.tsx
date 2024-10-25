58a9288aa467431394a1b170c35b157321070b87
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-10-25 15:01:30 +0500

    feat: added components AppLink, Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import { Link, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import MainPage from "./pages/MainPage"
 import ProfilePage from "./pages/ProfilePage"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 const App = () => {
     return (
         <div className={'app'}>
-            <div>
-                <Link to={'/'}>main</Link>
-                <Link to={'/profile'}>profile</Link>
-            </div>
-            <div>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                </Routes>
+            <div className={'wrapper'}>
+                <Sidebar />
+                <div className={'content'}>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/profile" element={<ProfilePage />} />
+                    </Routes>
+                </div>
             </div>
         </div>
     )
