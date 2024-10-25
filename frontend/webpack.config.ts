ae5d984d189ff6906d7579659371d6e791587992
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-10-25 15:10:40 +0500

    fix: decomposed webpack config

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,71 +1,23 @@
 import path from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpackDevServer from 'webpack-dev-server';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 
-interface IEnv {
-    mode: 'development' | 'production'
-}
-
-export default (env: IEnv) => {
-    const mode = env.mode || 'development'
-    const isDev = mode === 'development'
-
-    const config: webpack.Configuration = {
-        mode: 'development',
+export default (env: BuildEnv) => {
+    const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        output: {
-            path: path.resolve(__dirname, 'build'),
-            filename: '[name].[contenthash:8].js',
-            clean: true
-        },
-        devtool: 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: {
-                                    auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                                    localIdentName: isDev
-                                        ? '[path][name]__[local]--[hash:base64:5]'
-                                        : '[hash:base64:8]',
-                                },
-                            },
-                        },
-                        "sass-loader",
-                    ],
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public', 'index.html'),
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-        ],
-        devServer: {
-            port: 3000,
-            open: true,
-            historyApiFallback: true
-        }
-    }
+        build: path.resolve(__dirname, 'dist'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    };
+
+    const mode = env.mode || 'development';
+    const port = env.port || 3000;
+    const isDev = mode === 'development';
+    const config = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port,
+    });
 
     return config
 }
