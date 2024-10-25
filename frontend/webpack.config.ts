9a302fd9e6cd02a71e012d0f1d6243fd160cbfa5
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-10-25 13:01:17 +0500

    feat: installed typescript

---
+++ frontend/webpack.config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash:8].js',
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
+}
+
+export default config;
