0aea2aca352403d1e9e60b0b5784ef692c45dbc9
Bekzat Abdibakas <babdibakas@yandex.kz>
2024-10-26 16:09:01 +0500

    feat: added ui kit AppInput

--- frontend/src/pages/MainPage/ui/MainPage.tsx
+++ frontend/src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
 import AppButton from "shared/ui/AppButton/AppButton"
 import { AppHeader } from "shared/ui/AppHeader/AppHeader"
+import AppInput from "shared/ui/AppInput/AppInput"
 import { AppModal } from "shared/ui/AppModal/AppModal"
 
 const MainPage = () => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+    const [username, setUsername] = useState<string>('')
 
     const onModalOpen = () => {
         setIsModalOpen(true)
     }
     const onModalClose = () => {
         setIsModalOpen(false)
+    }
+    const onSetUsername = (value: string) => {
+        setUsername(value)
     }
 
     return (
@@ -23,7 +28,11 @@ const MainPage = () => {
             {
                 isModalOpen && (
                     <AppModal isOpen={isModalOpen} onClose={onModalClose}>
-                        Hello, World
+                        <AppInput
+                            value={username}
+                            onChange={onSetUsername}
+                            placeholder="Username"
+                        />
                     </AppModal>
                 )
             }
